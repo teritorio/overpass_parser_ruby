da7339ca14a3143da319c95f32afc2aeef1650d4
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2025-06-28 16:01:49 +0200

    Update

--- test/overpass_parser_ruby_test.rb
+++ test/overpass_parser_ruby_test.rb
@@ -5,6 +5,13 @@ class OverpassParserRubyTest < Minitest::Test
   # Test parsing a sample Overpass QL query
   def test_parse
     query = <<~QUERY
+      // @name Drinking Water
+
+      /*
+      This is an example Overpass query.
+      Try it out by pressing the Run button above!
+      You can find more examples with the Load tool.
+      */
       [out:json];
       node["amenity"="cafe"](50.7,7.1,50.8,7.2);
       out body;
